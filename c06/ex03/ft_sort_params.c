3c86849e92c7f69a71e732650bc7ccf0917c9029
Volodymyr Dukhanin <116935261+VD-88@users.noreply.github.com>
2023-07-25 17:30:10 +0200

    Add files via upload
    c06 fixed

--- c06/ex03/ft_sort_params.c
+++ c06/ex03/ft_sort_params.c
@@ -6,11 +6,12 @@
 /*   By: vdukhani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 22:04:31 by vdukhani          #+#    #+#             */
-/*   Updated: 2023/07/24 23:06:46 by vdukhani         ###   ########.fr       */
+/*   Updated: 2023/07/25 16:09:11 by vdukhani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
 void	print(int argc, char *argv[])
 {
@@ -30,38 +31,45 @@ void	print(int argc, char *argv[])
 	}
 }
 
-int	main(int argc, char *argv[])
+void	sorting(int counter, char **argv)
 {
-	int	i;
-	int	j;
-	char *temp;
-	int	counter;
-   
-	counter	= argc;
-	i = 0;
-	j = 0;
-	while (argc > 1)
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 1;
+	while (counter > 2)
 	{
 		j = 0;
-		while (argv[i][j])
-		{	
+		while (argv[i][j] || argv[i + 1][j])
+		{
 			if (argv[i][j] == argv[i + 1][j])
 				j++;
-			else
+			else if (argv[i][j] > argv[i + 1][j])
 			{
-				if (argv[i][j] < argv[i + 1][j])
-				{	
-					temp = argv[i];
-					argv[i] = argv[i + 1];
-					argv[i + 1] = temp;
-					break;
-				}
-				j++;
+				temp = argv[i];
+				argv[i] = argv[i + 1];
+				argv[i + 1] = temp;
+				break ;
 			}
+			else if (argv[i][j] < argv[i + 1][j])
+				break ;
 		}
 		i++;
-		argc--;
+		counter--;
 	}
-	print(counter, argv);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	k;
+
+	k = 0;
+	while (k < argc - 1)
+	{
+		sorting(argc, argv);
+		k++;
+	}
+	print(argc, argv);
 	return (0);
 }
